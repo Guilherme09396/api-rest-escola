c377b8726bd8c23aeedd60cdc7226525b5219d02
Guilherme Gomes <guilherme09396@gmail.com>
2023-01-31 09:26:07 -0300

    Rota de update e delete finalizadas, middleware de login adicionado nas rotas para só editar e deletar o usuário em questão

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,9 @@
-import UserModel from '../models/UserModel';
+import User from '../models/UserModel';
 
 class UserController {
   async index(req, res) {
     try {
-      const users = await UserModel.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (err) {
       return res.json(err);
@@ -12,14 +12,13 @@ class UserController {
 
   async store(req, res) {
     try {
-      const { nome, email, password } = req.body;
-      if (!nome || !email || !password) {
+      if (!req.body.nome || !req.body.email || !req.body.password) {
         return res.json({
           errors: ['Informe todos os campos necesários (nome, email, password)'],
         });
       }
-      const user = await UserModel.create(req.body);
-      return res.json(user);
+      const { id, nome, email } = await User.create(req.body);
+      return res.json({ id, nome, email });
     } catch (err) {
       if (err.errors) {
         return res.json({
@@ -34,7 +33,44 @@ class UserController {
   }
 
   async update(req, res) {
-    res.send('Olá');
+    const { userId } = req;
+    try {
+      const user = await User.findByPk(userId);
+      if (!user) {
+        return res.json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const { id, nome, email } = await user.update(req.body);
+      return res.json({ id, nome, email });
+    } catch (err) {
+      if (err.errors) {
+        return res.json({
+          errors: err.errors.map((error) => error.message),
+        });
+      }
+      return res.json({
+        errors: [err],
+      });
+    }
+  }
+
+  async delete(req, res) {
+    const id = req.userId;
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json({ apagado: true });
+    } catch (err) {
+      return res.json({ errors: [err] });
+    }
   }
 }
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,11 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = Router();
 
-router.post('/', loginRequired, UserController.store);
 router.get('/', UserController.index);
+router.post('/', loginRequired, UserController.store);
+router.put('/', loginRequired, UserController.update);
+router.delete('/', loginRequired, UserController.delete);
 
 export default router;
+
+// rota delete finalizada
